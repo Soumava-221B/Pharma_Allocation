bb6687b81b5a4fde2c725408e23e7fcd0cec834a
Soumava Das <soumavadas06@gmail.com>
2023-04-18 10:49:43 +0000

    i

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -30,7 +30,7 @@ const Home = () => {
 
       <div style={{ position: "absolute", bottom: 0, width:"100%" }} className="bg-gray-100">
         <div className="bg-gray-100 container mx-auto px-6 pt-10 pb-6">
-            © Pharma Allocation. All rights reserved.
+            © MedBridge. All rights reserved.
     </div>
   </div>
     </Layout>
